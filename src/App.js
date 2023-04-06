a5cef9ca561bc3cefb3674ecb274ce51744beeb8
prth8564 <91619019+prth8564@users.noreply.github.com>
2023-04-06 12:34:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 function App() {
   const [data,setData]=useState({})
   const [location,setLocation]=useState('')
-  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=4375c7281bf6cfeb3d7b0060ddcdbb24`
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid={api_key}`
   //4375c7281bf6cfeb3d7b0060ddcdbb24
   const searchLocation =(event)=>{
     if(event.key=='Enter'){
